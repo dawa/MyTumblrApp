9a6a46f21ad7862df8aca57f28577f32b56bf168
Gauri Tikekar <gauritikekar@Gauris-MacBook-Pro.local>
2017-03-29 19:27:06 -0700

    Tumblr project first milestone
    created empty view

---
+++ MyTumblrApp/Pods/Target Support Files/Pods-MyTumblrApp/Pods-MyTumblrApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyTumblrApp : NSObject
+@end
+@implementation PodsDummy_Pods_MyTumblrApp
+@end
---
+++ MyTumblrApp/Pods/Target Support Files/Pods-MyTumblrAppTests/Pods-MyTumblrAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyTumblrAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyTumblrAppTests
+@end
